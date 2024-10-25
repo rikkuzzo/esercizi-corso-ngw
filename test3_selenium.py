b54564d14fa7f6685a21d6f485bbe9ca5da50b56
rikkuzzo <terranovariccardo06@gmail.com>
2024-10-25 16:09:19 +0200

    .

--- test3_selenium.py
+++ test3_selenium.py
@@ -11,30 +11,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, WebDriverException
 
-# Configurazione del logging
 logging.basicConfig(level=logging.INFO)
 
-# Inserisci qui il token del bot Telegram
+
 API_TOKEN = '7954433263:AAFjwI7-kNReRU-CvX5zu4xaOgzHky2ieok'
 
-# Crea un'istanza del bot
 bot = telebot.TeleBot(API_TOKEN)
 
-# Funzione per cercare prodotti su Amazon
+
 def cerca_prodotti_su_amazon(prodotto_da_cercare):
     options = Options()
-    options.add_argument("--headless")  # Modalità headless, non apre il browser visivamente
+    options.add_argument("--headless")  #non apre il browser visivamnete 
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
     driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     try:
-        # Vai alla pagina di Amazon
+        #va su amazon 
         driver.get("https://www.amazon.it")
-        time.sleep(2)  # Attendi il caricamento della pagina
+        time.sleep(2)  
 
-        # Accetta i cookies, se necessario
+        #Accetta i cookies 
         try:
             cookies_button = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.ID, "sp-cc-accept"))
@@ -44,17 +42,16 @@ def cerca_prodotti_su_amazon(prodotto_da_cercare):
         except (NoSuchElementException, TimeoutException):
             logging.warning("Il bottone dei cookies non è stato trovato o non è cliccabile.")
 
-        # Cerca il prodotto
+    
         search_box = driver.find_element(By.ID, "twotabsearchtextbox")
         search_box.send_keys(prodotto_da_cercare)
         search_box.send_keys(Keys.RETURN)
 
-        # Attendi il caricamento della pagina dei risultati
+ 
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, ".s-main-slot"))
         )
 
-        # Estrai i prodotti con titolo e prezzo
         prodotti = []
         products = driver.find_elements(By.CSS_SELECTOR, ".s-main-slot .s-result-item")
 
